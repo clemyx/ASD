728aabc53b2f58295dbf16a6211ee5ec6d0cd4bb
clemyx <delgrossoclemyx@gmail.com>
2025-11-01 12:06:49 +0100

    Update student test with new course data

--- lab ASD/lab4/TestStudente.cpp
+++ lab ASD/lab4/TestStudente.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int main(){
     studente s1;
-    studente s2("Statistica","Psicologia Clinica","Psicometria","Psicologia delle comunicazioni sociali","Criminologia");
-    s2.setMatricola("761173");
-    s2.setNome("Maria");
-    s2.setEsame(29,1);
-    s2.setEsame(22,2);
-    s2.setEsame(30,3);
-    s2.setEsame(30,4);
+    studente s2("Algoritmi e Strutture Dati","Programmazione","Analisi Matematica","Ingegneria del Software","Linguaggi di Programmazione");
+    s2.setMatricola("863945");
+    s2.setNome("Clemyx");
+    s2.setEsame(27,1);
+    s2.setEsame(20,2);
+    s2.setEsame(18,4);
     cout<<endl<<endl;
     s2.printStudente();
     cout<<endl;
